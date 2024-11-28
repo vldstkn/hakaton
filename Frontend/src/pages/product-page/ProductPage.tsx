0d982b8ac14a144b85eede9f8f556c724e1a22ea
Anastasia-Kiseleva12 <pm15.kiseleva1@gmail.com>
2024-11-28 22:09:35 +0300

    pr page

--- Frontend/src/pages/product-page/ProductPage.tsx
+++ Frontend/src/pages/product-page/ProductPage.tsx
@@ -1,22 +1,51 @@
 import { useParams } from 'react-router-dom';
 import './productPage.css'
-import { Button } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 import { useState } from 'react';
-
-
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
 
 export default function ProductPage() {
     //Получение идентификатора товара из url
     const { id } = useParams();
     const [count, setCount] = useState(0)
 
+  
+
     return (
     <div className="productPage">
-    <h1>Страница товара {id} </h1>
-    <div>
-      Пример рандомной кнопки 
-      <Button  className='button' onClick={() => setCount((count) => count + 1)} variant="contained">{count}</Button>
+    <div className="carousel">
+          <img
+                src="/product.jpg"
+                style={{
+                    maxWidth: '100%',
+                    height:'100%',
+                    // width: '100%', // Задаем ширину 100% от родительского элемента
+                    // height: '100%', // Задаем высоту 100% от родительского элемента
+                    // objectFit: 'cover', // Масштабируем изображение, сохраняя его пропорции
+                }}
+            />
       </div>
+    <div className="productCard">
+      <h1>Название товара</h1>
+      <div className="prAbout"> <h3>Описание товара</h3></div>  
+      <div className="prPriceButton">
+      <div className="prPrice"><h3>Прайс</h3></div>
+      <div className="buttonContainer">
+        <Button className="buttonbuy"
+            variant="contained"
+            endIcon={<CreditCardIcon />}>
+            Купить сейчас
+      </Button>
+      <Button className="buttoncart"
+            variant="contained"
+            endIcon={<ShoppingCartIcon />}>
+            Добавить в корзину
+      </Button>
+      </div>
+      </div>
+    </div>
+      
     </div>
     )
   }
