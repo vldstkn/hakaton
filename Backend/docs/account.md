e3338dd2787fd062ebf29da26f578361c0663042
Vlad <vldstkn.develop@gmail.com>
2024-11-30 03:04:02 +0700

    Fix add product list

--- Backend/docs/account.md
+++ Backend/docs/account.md
@@ -89,3 +89,4 @@ url: ***/account/profile***
 	"name": "vlad"
 }
 ```
+cd
--- Backend/golang/internal/products/repository.go
+++ Backend/golang/internal/products/repository.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"hakaton/internal/domain"
 	"hakaton/pkg/db"
+	"strings"
 )
 
 type Repository struct {
@@ -18,44 +19,37 @@ func NewRepository(database *db.DB) *Repository {
 }
 
 func (repo *Repository) AddMultiple(products []domain.Product) error {
-	//var valueStrings []string
-	//var valueArgs []interface{}
+	var valueStrings []string
+	var valueArgs []interface{}
 	//FIXME:
 	vector := make([]float64, 312)
 	vecStr := vectorToString(vector)
 
-	query := `INSERT INTO products (embedding) VALUES ($1)`
-	_, err := repo.DB.Exec(query, vecStr)
+	for i, product := range products {
+		valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d)",
+			i*8+1, i*8+2, i*8+3, i*8+4, i*8+5, i*8+6, i*8+7, i*8+8))
 
+		valueArgs = append(valueArgs, product.Price, product.Rating, product.NumberReviews,
+			product.Link, product.CatId, product.Name, product.Description, vecStr)
+	}
+
+	query := fmt.Sprintf(`
+		INSERT INTO products (price, rating, number_reviews, link, cat_id, name, description, embedding)
+		VALUES %s`, strings.Join(valueStrings, ", "))
+
+	_, err := repo.DB.Exec(query, valueArgs...)
 	return err
-
-	//for i, product := range products {
-	//	valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d)",
-	//		i*8+1, i*8+2, i*8+3, i*8+4, i*8+5, i*8+6, i*8+7, i*8+8))
-	//
-	//	valueArgs = append(valueArgs, product.Price, product.Rating, product.NumberReviews,
-	//		product.Link, product.CatId, product.Name, product.Description, vecStr)
-	//}
-	//
-	//query := fmt.Sprintf(`
-	//	INSERT INTO products (price, rating, number_reviews, link, cat_id, name, description, embedding)
-	//	VALUES %s`, strings.Join(valueStrings, ", "))
-	//
-	//fmt.Println(valueArgs...)
-	//
-	//_, err := repo.DB.Exec(query, valueArgs...)
-	//return err
 }
 
 func vectorToString(vector []float64) string {
 	var buf bytes.Buffer
-	buf.WriteString("{")
+	buf.WriteString("[")
 	for i, v := range vector {
 		if i > 0 {
 			buf.WriteString(",")
 		}
 		buf.WriteString(fmt.Sprintf("%f", v))
 	}
-	buf.WriteString("}")
+	buf.WriteString("]")
 	return buf.String()
 }
