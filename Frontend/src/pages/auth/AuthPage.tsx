48abb1659774533aec2a77869493a2a8782cf7c8
Anastasia-Kiseleva12 <pm15.kiseleva1@gmail.com>
2024-11-30 13:01:33 +0300

    new design auth

--- Frontend/src/pages/auth/AuthPage.tsx
+++ Frontend/src/pages/auth/AuthPage.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import styles from './AuthPage.module.css'
 import { useForm, SubmitHandler  } from 'react-hook-form'
 import toast from 'react-hot-toast'
-import { Button } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 
 type AuthForm = {
     email: string
@@ -24,18 +24,16 @@ export default function AuthPage(){
 
 
     return <div className={styles['page']}>
-        <div className={styles['form-wrap']}>
             <h2 className={styles['title']}>{isLogin ? "Авторизация" : "Регистрация"}</h2>
             <form className={styles['form']} onSubmit={handleSubmit(onSubmit)}>
-                <input className={styles['input']} placeholder='email' type='email' {...register("email", { required: true })}/>
-                <input className={styles['input']} placeholder='password' type='password' {...register("password", { required: true })}/>
-                {!isLogin && <input className={styles['input']} placeholder='name' type='text' {...register("name", { required: true })}/>}
+            <TextField className={styles['input']} id="standard-basic" label="email" variant="standard"  type='email' {...register("email", { required: true })} />
+            <TextField className={styles['input']} id="standard-basic" label="password" variant="standard"  type='password' {...register("password", { required: true })} />
+               {!isLogin && <TextField className={styles['input']} id="standard-basic" variant="standard" label='name' type='text' {...register("name", { required: true })}/>}
                 <button className={styles['button']}>{isLogin ? "войти" : "создать"}</button>
                 {(errors.email || errors.password || errors.name) && <span className={styles['error']}>Все поля обязательны для заполнения</span>}
             </form>
             <button  className={styles['btn-set-type']} onClick={() => setIsLogin(!isLogin)}>
                 {isLogin ? "зарегестрироваться" : "войти"}
             </button>
-        </div>
     </div>
 }
