791d364f0275601639e1d8809712e9d0124b03f1
Solovev Viacheslav <solovey.1999@gmail.com>
2024-12-01 04:25:56 +0700

    Add GetAllProducts contract

---
+++ Backend/csharp/Contracts/Product/GetAllProducts.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Contracts.Product
+{
+    public record GetAllProductsResponse(
+        IEnumerable<Product> Products);
+
+    public record Product(
+        int Id,
+        int Price,
+        int Rating,
+        string Link,
+        string Name,
+        string Description,
+        [property: JsonProperty("created_at")] DateTime CreatedAt,
+        [property: JsonProperty("updated_at")] DateTime UpdatedAt,
+        [property: JsonProperty("number_reviews")] int NumberReviews,
+        [property: JsonProperty("cat_id")] int CategoryId);
+}
